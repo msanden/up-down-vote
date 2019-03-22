7e0e07f36c31245f1d6ea6b3ab0da0804071c218
Manuel Sande <mnlsande22@gmail.com>
2019-03-22 06:50:41 -0400

    Import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListQuoteComponent } from './list-quote/list-quote.component';
@@ -12,7 +13,8 @@ import { NewQuoteComponent } from './new-quote/new-quote.component';
     NewQuoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
