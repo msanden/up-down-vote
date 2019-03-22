87c490bba97c18373f62d6d197e04bd8198e7673
Manuel Sande <mnlsande22@gmail.com>
2019-03-22 05:00:11 -0400

    Import & Declare quote-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListQuoteComponent } from './list-quote/list-quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListQuoteComponent
   ],
   imports: [
     BrowserModule
