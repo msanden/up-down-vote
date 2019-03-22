d6e748a09dc796e9155f22bdc8d35abc4d11b365
Manuel Sande <mnlsande22@gmail.com>
2019-03-22 03:38:58 -0400

    Import Quote class, Create a master quote list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './models/quote.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'upvote-downvote-app';
+  title = 'Upvote or Downvote a Quote';
+
+  masterQuoteList: Quote[] = [
+    new Quote(`Happiness is when what you think, what you say, and what you do
+     are in harmony. Mahatma Gandhi`),
+    new Quote(`I have decided to stick with love. Hate is too great a burden to
+     bear. Martin Luther King Jr.`),
+    new Quote(`I learned that courage was not the absence of fear, but the
+     triumph over it. The brave man is not he who does not feel afraid, but he
+     who conquers that fear. Nelson Mandela`),
+    new Quote(`Let us always meet each other with smile, for the smile is the
+     beginning of love. Mother Teresa`),
+    new Quote(`I'm not a 'Business-Man'! I'm a Business... man! Let me handle
+     my business, damn! Jay-Z`)
+  ];
 }
