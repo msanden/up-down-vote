29777daa9c0d86d384fee187a4cc527770f7cda6
Manuel Sande <mnlsande22@gmail.com>
2019-03-22 13:07:36 -0400

    Import and Declare new edit-quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListQuoteComponent } from './list-quote/list-quote.component';
 import { NewQuoteComponent } from './new-quote/new-quote.component';
+import { EditQuoteComponent } from './edit-quote/edit-quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListQuoteComponent,
-    NewQuoteComponent
+    NewQuoteComponent,
+    EditQuoteComponent
   ],
   imports: [
     BrowserModule,
