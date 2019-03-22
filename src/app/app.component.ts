c5cd4be3c11d0aab273fc5ebb77e118aba67f771
Manuel Sande <mnlsande22@gmail.com>
2019-03-22 05:00:45 -0400

    Add author-property to Quote object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,15 @@ export class AppComponent {
 
   masterQuoteList: Quote[] = [
     new Quote(`Happiness is when what you think, what you say, and what you do
-     are in harmony. Mahatma Gandhi`),
+     are in harmony.`, `Mahatma Gandhi`),
     new Quote(`I have decided to stick with love. Hate is too great a burden to
-     bear. Martin Luther King Jr.`),
+     bear.`, `Martin Luther King Jr.`),
     new Quote(`I learned that courage was not the absence of fear, but the
      triumph over it. The brave man is not he who does not feel afraid, but he
-     who conquers that fear. Nelson Mandela`),
+     who conquers that fear.`, `Nelson Mandela`),
     new Quote(`Let us always meet each other with smile, for the smile is the
-     beginning of love. Mother Teresa`),
+     beginning of love.`, `Mother Teresa`),
     new Quote(`I'm not a 'Business-Man'! I'm a Business... man! Let me handle
-     my business, damn! Jay-Z`)
+     my business, damn!`, `Jay-Z`)
   ];
 }
